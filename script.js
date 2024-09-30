2cee38f578c5c325d967a9f1a63910bf0d0fdf98
Jecksi Loewenthal <loewenthal01@gmail.com>
2024-09-30 19:01:54 -0300

    modificacion js

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
-console.log("Mixis starto!");
-
-let rascador = 1000;
-let casaDeGato = 500;
+let rascador = 5000;
+let casaDeGato = 20000;
 let jugueteDeGato = 2000;
-let comederoDeGato = 1223123;
+let comederoDeGato = 10000;
 
 let totalDeCompra = 0;
+
+console.log(
+  "1.- Rascador: " + rascador + "\n",
+  "2.- Casa de Gato " + casaDeGato + "\n",
+  "3.- Juguete de Gato " + jugueteDeGato + "\n",
+  "4.- Comedero de Gato " + comederoDeGato + "\n"
+);
 
 function agregarProductos(producto) {
   if (producto === "1") {
     alert("Se ha agregado el rascador a tu carrito de compra");
     totalDeCompra += rascador;
+    // totalDeCompra = 5000
   } else if (producto === "2") {
     alert("Se ha agregado la casa de gato a tu carrito de compra");
     totalDeCompra += casaDeGato;
+    // Total de compra ya pasa a ser 5000
+    // si el cliente compra la casa de gato se suma la variable de casa de gato al Total de compra
+    // pasaria a ser 25000
   } else if (producto === "3") {
     alert("Se ha agregado el juguete de gato a tu carrito de compra");
     totalDeCompra += jugueteDeGato;
@@ -32,6 +41,6 @@ while (continuar) {
   );
   agregarProductos(seleccion);
   continuar = confirm("Quieres continuar?");
-  console.log(seleccion);
 }
-console.log(alert("El total de tu compra es " + totalDeCompra));
+alert("El total de tu compra es " + totalDeCompra);
+console.log("El total de tu compra es " + totalDeCompra);
